58960a095a965b70c7e4096a62bac4c7770387ea
Gibson <gibsonthdev@gmail.com>
2022-02-12 11:46:31 +0700

    add render check

--- mini-rowy/pages/index.tsx
+++ mini-rowy/pages/index.tsx
@@ -1,27 +1,18 @@
 import type { NextPage } from "next";
 
-import React, { useState } from "react";
 import Button from "@/core/Button/_Button";
 import { default as Layout } from "@/layout/Home";
 import Table from "src/components/Table/_Table";
+import React from "react";
 import {
-  readTableColumnsAtom,
-  readTableDataAtom,
   updateTableColumnsAtom,
   updateTableDataAtom,
 } from "@/store/TableState/helper";
 import RenderCheck from "src/components/RenderCheck";
-import { setUncaughtExceptionCaptureCallback } from "process";
-import NoMemo from "src/components/RenderCheck/NoMemo";
-import { useUpdateAtom } from "jotai/utils";
-import { tableColumnAtom, tableDataAtom } from "./_app";
 
 const Home: NextPage = () => {
-  const [text, setText] = useState("");
-  const [text2, setText2] = useState("");
-
-  const updateTableData = useUpdateAtom(tableDataAtom);
-  const updateTableColumns = useUpdateAtom(tableColumnAtom);
+  const { updateTableColumns } = updateTableColumnsAtom();
+  const { updateTableData } = updateTableDataAtom();
 
   const handleAddColumn = () => {
     updateTableColumns((prev) => {
@@ -36,32 +27,16 @@ const Home: NextPage = () => {
 
   const handleAddRow = () => {
     updateTableData((prev: any) => {
-      return [...prev, {}];
+      return [{}, ...prev];
     });
   };
-
-  const passAnon = React.useCallback(() => setText(""), [setText]);
-  const helloWolrd = () => console.log("hello world");
-  const dataIssue = { haveData: false }; //on rerender, a new reference is created
 
   return (
     <Layout>
       <Table />
       <Button title="Add Column" onClick={handleAddColumn} />
       <Button title="Add Row" onClick={handleAddRow} />
-      {/** This is testing purpose */}
-      <input
-        placeholder={text}
-        value={text}
-        onChange={(e: any) => setText(e?.target?.value)}
-      />
-      <input
-        placeholder={text2}
-        value={text2}
-        onChange={(e: any) => setText2(e?.target?.value)}
-      />
-      <RenderCheck passAnon={helloWolrd} data={dataIssue} />
-      <NoMemo />
+      <RenderCheck />
     </Layout>
   );
 };
